4fb6ea8b8d5c138be2673862eab7b84123d0a5c9
MinuNayS <minu.varghese@ust.com>
2024-12-17 00:23:38 +0530

    TASK update

--- controllers/taskController.go
+++ controllers/taskController.go
@@ -18,3 +18,29 @@ func CreateTask(c *gin.Context){
 	}
 	c.JSON(http.StatusOK, gin.H{"message":"Task created successfully"})
 }
+func UpdateTask(c *gin.Context){
+	var task models.Task
+	if err:=c.ShouldBindJSON(&task);err !=nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+		return
+	}
+	_,err := database.DB.NamedExec(`UPDATE TASKS SET =$1title=$1, description=$2, status=$3, priority=$4, deadline=$5 where project_id=$6 and user_id=$7)`,&task)
+	if err !=nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":"Error Updating task"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message":"Task Updated successfully"})
+}
+func DeleteTask(c *gin.Context){
+	var task models.Task
+	if err:=c.ShouldBindJSON(&task);err !=nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+		return
+	}
+	_,err := database.DB.NamedExec(`DELETE FROM TASKS WHERE project_id=$1 AND user_id=$2)`,&task)
+	if err !=nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":"Error creating task"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message":"Task created successfully"})
+}
--- main.go
+++ main.go
@@ -13,7 +13,11 @@ func main() {
 	r.POST("/login", controllers.Login)
 	taskRoutes := r.Group("/tasks")
 	{
-		taskRoutes.POST("/", controllers.CreateTask)
+		taskRoutes.POST("/tasks", controllers.CreateTask)
+		taskRoutes.GET("/tasks", controllers.GetTask)
+		taskRoutes.PUT("/tasks/:id", controllers.UpdateTask)
+		taskRoutes.DELETE("/tasks/:id", controllers.DeleteTask)
+
 	}
 	r.Run(":8080")
 
